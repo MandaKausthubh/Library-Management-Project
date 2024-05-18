7f54bb4fac6a439ff6f8dab14d5cdb8e261925bb
Manda Kausthubh <kausthubhh.manda@gmail.com>
2024-05-18 23:03:36 +0530

    Rewriting the whole project

--- Server.c
+++ Server.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <string.h>
-
+#include <unistd.h>
+#include <fcntl.h>
 const char LibrarianFile[ ] = "Librarian.bin";
 
 typedef struct {
@@ -9,45 +10,62 @@ typedef struct {
     char Password[30];
 } LoginIDPass;
 
-int main(void) {
-    // First Create a Login:
-    // Login: Option 1:Login, Option 2: Create a new account.
-    // Login:
-    //      Ask for LoginID, and Password.
-    //      Retrieve the LoginID and Password from the Librarian.bin.
-    //      Check if it is correct, then proceed.
-    //      else Send message: Invalid ID (or) Invalid Password.
+
+void Login() {
     while(1) {
         char LoginID[30], Password[30];
         printf("Please enter your Librarian ID: ");
         scanf("%s", LoginID);
         printf("Please enter your Librarian Password: ");
         scanf("%s", Password);
-
         printf("\n\n\n");
 
-        FILE* fptr = fopen(LibrarianFile, "rb+");
-
+        int fd = open(LibrarianFile, O_RDONLY);
         LoginIDPass Temp;
         int login = 0;
 
-        while(fread(&Temp, sizeof(Temp), 1, fptr) != 0) {
+        while(1) {
+            read(fd, &Temp.ID, sizeof(Temp));
             if(strcmp(Temp.ID, LoginID) == 0) {
                 if(strcmp(Temp.Password, Password)) {
                     printf("Login Successful !!\n");
                     login = 1;
                 } else {
                     printf("Wrong Password Entered!!\n");
+                    Login();
                 }
                 break;
             }
         }
-
-        if(login) break;
+        close(fd);
+        if(login) return;
+        Login();
     }
+}
 
-    // Create a TCP Socket which is used to create new thread and service the 
-    int sockfd = socket(AF_LOCAL, SOCK_STREAM, 0);
+void CreateNewAccount() {
+    char LoginID[30], Password[30];
+    printf("New LibrarianID: ");
+    scanf("%s", LoginID);
+    printf("New password");
+    scanf("%s", Password);
+    int fd = open(LibrarianFile, O_RDWR);
+    LoginIDPass Temp;
+    int login = 0;
+
+    while(read(fd, &Temp, sizeof(Temp)) != 0) {
+        if(strcmp(Temp.ID, LoginID) == 0) {
+            printf("Account aldready exists!!");
+            login = 1;
+            break;
+        }
+    }
+    close(fd);
+    if(login) return;
     
+}
+
+int main(void)  {
+    Login();
     return 0;
 }
